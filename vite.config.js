145ecf5750244dc4b345199a70b1c2edab78fc93
iman alizadeh <imonilol01@gmail.com>
2025-02-09 09:38:15 +0330

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-map-import-export/react-map/',
+  base: '/react-map-import-export/',
 })
 
