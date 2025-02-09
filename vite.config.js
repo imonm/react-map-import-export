7f9ef2ed6645f6a93a0c7a5f62eafb999b7739c5
iman alizadeh <imonilol01@gmail.com>
2025-02-09 10:05:19 +0330

    سش

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/react-map-import-export',
+  base: '/react-map-import-export/', // برای GitHub Pages ضروریه
   build: {
-    outDir: 'build',
-  },
-  // سایر تنظیمات...
-});
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
+})
